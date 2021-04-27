18af4847b3506be908fc8b7ccb685736055aca2e
khanhcn1999 <83207166+khanhcn1999@users.noreply.github.com>
2021-04-27 10:07:40 +0700

    Add files via upload

---
+++ DTO/HoaDon.java
@@ -0,0 +1,68 @@
+package DTO;
+
+public class HoaDon {
+    private String MaHD;
+    private String MaTV;
+    private String MaNV;
+    private String MaKM;
+    private String NgayLHD;
+    private double TongTien;
+    private double TongKM;
+    private double TongTT;
+
+    public String getMaHD() {
+        return MaHD;
+    }
+    public void setMaHD(String maHD) {
+        MaHD = maHD;
+    }
+    
+    public String getMaTV() {
+        return MaTV;
+    }
+    public void setMaTV(String maTV) {
+        MaTV = maTV;
+    }
+    
+    public String getMaNV() {
+        return MaNV;
+    }
+    public void setMaNV(String maNV) {
+        MaNV = maNV;
+    }
+    
+    public String getMaKM() {
+        return MaKM;
+    }
+    public void setMaKM(String maKM) {
+        MaKM = maKM;
+    }
+    
+    public String getNgayLHD() {
+        return NgayLHD;
+    }
+    public void setNgayLHD(String ngayLHD) {
+        NgayLHD = ngayLHD;
+    }
+    
+    public double getTongTien() {
+        return TongTien;
+    }
+    public void setTongTien(double tongTien) {
+        TongTien = tongTien;
+    }
+    
+    public double getTongKM() {
+        return TongKM;
+    }
+    public void setTongKM(double tongKM) {
+        TongKM = tongKM;
+    }
+    
+    public double getTongTT() {
+        return TongTT;
+    }
+    public void setTongTT(double tongtt) {
+        TongTT = tongtt;
+    }
+}
